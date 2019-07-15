0ebe6e78b4c11a54b3d1d11fa539600de7c08704
pengchongfu <pengchongfu@gmail.com>
2019-07-16 00:41:02 +0800

    http-router support pattern

--- 架构/web技术/http-router.go
+++ 架构/web技术/http-router.go
@@ -1,58 +1,165 @@
+// support {} pattern like
+// - /api/user/{userid}/info
+// - /api/user/user-{userid}/info
+// not support
+// - /api/user/{userid}-user/info
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	patternStart = '{'
+	patternEnd   = '}'
+	separator = '/'
+)
 
 func main() {
 	t := &tree{}
-	t.insert("/aa/bb/cc", func() {
-		fmt.Println("aabbcc")
+	t.Insert("/api/user", func() {
+		fmt.Println("certain 1")
 	})
-	t.find("/aa/bb/cc")()
+	t.Insert("/api/user/123/info", func() {
+		fmt.Println("certain 2")
+	})
+	t.Insert("/api/user-{id}/info", func() {
+		fmt.Println("prefix pattern 1")
+	})
+	t.Insert("/api/user/user-{id}/info", func() {
+		fmt.Println("prefix pattern 2")
+	})
+	t.Insert("/api/user/{userid}/info", func() {
+		fmt.Println("pattern")
+	})
+	t.Insert("/api/user/456/info", func() {
+		fmt.Println("certain 3")
+	})
+	t.Insert("/api/unit", func(){
+		fmt.Println("certain 4")
+	})
+	print(t)
+	fmt.Println()
 
+	fmt.Println(t.Find("/api/unit/") == nil)
+	fmt.Println()
+
+	t.Find("/api/user")()
+	t.Find("/api/user/123/info")()
+	t.Find("/api/user-123/info")()
+	t.Find("/api/user/user-1234/info")()
+	t.Find("/api/user/1234/info")()
+	t.Find("/api/user/456/info")()
+	t.Find("/api/unit")()
 }
 
 type tree struct {
-	children map[string]*tree
-	handler  func()
+	children  []*tree
+	char      byte
+	isPattern bool
+	handler   func()
 }
 
-func (t *tree) insert(path string, handler func()) {
-	low, high := 1, 1
-	for high < len(path) {
-		for high < len(path) && path[high] != '/' {
-			high++
+func hasPattern(path string, index int) int {
+	if path[index] != patternStart {
+		return -1
+	}
+	for i := index + 1; i < len(path); i++ {
+		if path[i] == patternEnd {
+			return i
+		}
+	}
+	return -1
+}
+
+func (t *tree) Insert(path string, handler func()) {
+	index := 0
+	for index < len(path) {
+		var nextTree *tree
+
+		if patternIndex := hasPattern(path, index); patternIndex != -1 {
+			for _, child := range t.children {
+				if child.isPattern {
+					nextTree = child
+				}
+				break
+			}
+			if nextTree == nil {
+				nextTree = &tree{isPattern: true}
+				t.children = append(t.children, nextTree)
+			}
+			index = patternIndex + 1
+		} else {
+			char := path[index]
+			for _, child := range t.children {
+				if child.char == char {
+					nextTree = child
+					break
+				}
+			}
+			if nextTree == nil {
+				nextTree = &tree{char: char}
+				t.children = append(t.children, nextTree)
+			}
+			index++
 		}
 
-		if t.children == nil {
-			t.children = map[string]*tree{}
-		}
-		if t.children[path[low:high]] == nil {
-			t.children[path[low:high]] = &tree{}
-		}
-
-		t = t.children[path[low:high]]
-		high++
-		low = high
+		t = nextTree
 	}
 
 	t.handler = handler
 }
 
-func (t *tree) find(path string) func() {
-	low, high := 1, 1
-	for high < len(path) {
-		for high < len(path) && path[high] != '/' {
-			high++
-		}
+func (t *tree) Find(path string) func() {
+	return t.find(path, -1)
+}
 
-		if t.children == nil || t.children[path[low:high]] == nil {
-			return nil
+func (t *tree) find(path string, index int) func() {
+	if t.isPattern {
+		for index+1 < len(path) && path[index+1] != separator {
+			index++
 		}
-
-		t = t.children[path[low:high]]
-		high++
-		low = high
+	} else {
+		if index != -1 && index < len(path) {
+			if path[index] != t.char {
+				return nil
+			}
+		}
 	}
 
-	return t.handler
+	if index == len(path)-1 {
+		return t.handler
+	}
+
+	for _, child := range t.children {
+		if handler := child.find(path, index+1); handler != nil {
+			return handler
+		}
+	}
+
+	return nil
+}
+
+func print(t *tree) {
+	helper(t, "")
+}
+
+func helper(t *tree, path string) {
+	if t == nil {
+		return
+	}
+	if t.isPattern {
+		path += string([]byte{patternStart, patternEnd})
+	} else {
+		path += string(t.char)
+	}
+
+	if t.handler != nil {
+		fmt.Println(path)
+	}
+
+	for _, child := range t.children {
+		helper(child, path)
+	}
 }
